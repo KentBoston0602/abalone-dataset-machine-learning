52c592f6cf56070b071e46cf8541c1bb5eb2b1aa
Kent Boston <kentcboston@gmail.com>
2024-11-10 18:31:48 +0800

    edited and finished the Dataset Page

--- abalone_dataset_project.py
+++ abalone_dataset_project.py
@@ -1,15 +1,27 @@
 #######################
-# Import libraries
+# Library Imports
+
+# Streamlit
 import streamlit as st
+
+# Data Analysis
 import pandas as pd
 import altair as alt
+
+# Data Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
 import plotly.express as px
+
+# Images
+from PIL import Image
 
 #######################
 # Page configuration
 st.set_page_config(
     page_title="Abalone Dataset", # Replace this with your Project's Title
-    page_icon="assets/icon.png", # You may replace this with a custom icon or emoji related to your project
+    page_icon="assets/icon/icon.png", # You may replace this with a custom icon or emoji related to your project
     layout="wide",
     initial_sidebar_state="expanded")
 
@@ -70,7 +82,7 @@ with st.sidebar:
 # Data
 
 # Load data
-dataset = pd.read_csv("data/abalone.csv")
+abalone_df = pd.read_csv("data/abalone.csv")
 
 #######################
 
@@ -89,7 +101,7 @@ if st.session_state.page_selection == "about":
     2. `EDA` - Exploratory Data Analysis of the Abalone dataset. The analysis involved examining data both individually and in pairs. Histograms and pie charts were used to understand the distribution of numerical and categorical features, respectively. Pair plots and heatmaps were employed to visualize relationships and correlations between numerical features. Box plots were used to identify and potentially address outliers in the dataset.
     3. `Data Cleaning / Pre-processing` - Data cleaning and pre-processing steps such as encoding the 'Sex' column and splitting the dataset into training and testing sets.
     4. `Machine Learning` - Training three supervised ML models: Linear Regression, Random Forest Regressor and Support Vector Regression. Includes model evaluation, model prediction visualizations and feature importance.
-    5. `Prediction` - Prediction page where users can input values to predict the age of an abalone using the trained models.
+    5. `Prediction` - Prediction page where users can input values to predict the age of abalone using the trained models.
     6. `Conclusion` - Summary of the insights and observations from the EDA and model training.
 
 
@@ -99,10 +111,72 @@ if st.session_state.page_selection == "about":
 elif st.session_state.page_selection == "dataset":
     st.header("📊 Dataset")
 
-    st.write("IRIS Flower Dataset")
-    st.write("")
+    st.markdown("""
 
-    # Your content for your DATASET page goes here
+    The Abalone dataset was uploaded to UCI Machine Learning Repository. It is a dataset used widely in machine learning.
+
+    From the original data, examples with missing values, primarily those with missing predicted values, were removed. The ranges of continuous values were scaled by dividing by 200 for use with an ANN.  
+
+    For each sample, eight features are measured: Sex, Length, Diameter, Height, Whole weight, Shucked weight, Viscera weight, and Shell weight. This dataset is commonly used to test regression techniques to predict the Age of the abalone. The same dataset that is used for this data science activity was uploaded to Kaggle by the user named Rodolfo Mendes.
+
+    **Content**  
+    The dataset has 4177 rows containing 8 numeric attributes that are related to abalone, the columns are as follows: Sex, Length, Diameter, Height, Whole weight, Shucked weight, Viscera weight, Shell weight and Age.
+
+    `Link:` https://www.kaggle.com/datasets/rodolfomendes/abalone-dataset            
+                
+    """)
+
+    col_abalone = st.columns((3, 3, 3), gap='medium')
+
+    # Define the new dimensions (width, height)
+    resize_dimensions = (500, 300)  # Example dimensions, adjust as needed
+
+    with col_abalone[0]:
+        abalone1_image = Image.open('assets/abalone_pictures/abalone1.jpg')
+        abalone1_image = abalone1_image.resize(resize_dimensions)
+        st.image(abalone1_image)
+
+    with col_abalone[1]:
+        abalone2_image = Image.open('assets/abalone_pictures/abalone2.jpg')
+        abalone2_image = abalone2_image.resize(resize_dimensions)
+        st.image(abalone2_image)
+
+    with col_abalone[2]:
+
+        abalone3_image = Image.open('assets/abalone_pictures/abalone3.jpg')
+        abalone3_image = abalone3_image.resize(resize_dimensions)
+        st.image(abalone3_image)
+
+    # Display the dataset
+    st.subheader("Dataset displayed as a Data Frame")
+    st.dataframe(abalone_df, use_container_width=True, hide_index=True)
+
+    st.write("""
+
+    The Abalone dataset contains 4,177 entries with nine columns, where each column represents a feature of an abalone.
+
+    1. **Sex:** The sex of the abalone ('M', 'F', or 'I' for immature).
+    2. **Length:** The longest shell measurement (in millimeters).
+    3. **Diameter:** The shell measurement perpendicular to the length (in millimeters).
+    4. **Height:** The height of the abalone with the meat included (in millimeters).
+    5. **Whole weight:** The whole abalone’s weight (in grams).
+    6. **Shucked weight:** The weight of the abalone's meat (in grams) after it has been removed from the shell.
+    7. **Viscera weight:** The weight of the gut of the abalone after bleeding (in grams).
+    8. **Shell weight:** The weight of the abalone’s dried shell (in grams).
+    9. **Age:** The age of an abalone represented by its number of rings plus 1.5, indicating the number of years it has lived.
+    """)
+
+    # Describe Statistics
+    st.subheader("Descriptive Statistics")
+    st.dataframe(abalone_df.describe(), use_container_width=True)
+
+    st.markdown("""
+    * No missing values in the dataset.
+    * All features are numerical except for Sex.
+    * Although the features are not normally distributed, they are close to normality.
+    * None of the features have a minimum of 0 except for Height (requires re-check).
+    * Each feature has a different scale range.
+    """)
 
 # EDA Page
 elif st.session_state.page_selection == "eda":
